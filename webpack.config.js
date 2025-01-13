2995e550b30a2b9c4a077bb1851f4f1aa4102a54
Syed Farhan Ali <108853419+FarhanAli2000@users.noreply.github.com>
2025-01-13 19:05:58 +0500

    Your commit message here

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     static: {
       directory: path.join(__dirname, "public/"),
     },
-    port: 3002,
+    port: 3000,
     historyApiFallback: true,
     onAfterSetupMiddleware: function () {
       // Open the browser after the dev server is up and running
